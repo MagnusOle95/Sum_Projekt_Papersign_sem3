cf39c01bf84d8a4fa50d970e3f6f6cb356b0f340
MikkelHauge <mikkelhauge84+github@gmail.com>
2022-11-18 09:17:37 +0100

    test og node_modules mapper tilføjet

---
+++ test/ordre.test.js
@@ -0,0 +1,21 @@
+const { assert } = require('chai');
+const { describe } = require('mocha');
+const logik = require('../logik.js');
+const ordre = require('../ordre.js');
+
+describe('Test af faktura', () => {
+    it('oprettelse af faktura', () => {
+        let faktura = new ordre.Faktura("Magnus"); //mangler underskrift
+        assert.isNotNull(faktura, false);
+        assert.equal(faktura.navn, "Magnus");
+        assert.equal(faktura.dato.getFullYear(), 2022);
+    })
+
+    it('oprettelse af ordrelinjer, på faktura', () => {
+        let faktura = new ordre.Faktura("Jens"); //mangler underskrift
+        let produktgruppe = logik.createProductgroup("Udklædning", "alt udklædning, til dine kinky tendenser")
+        let p1 = new logik.createProduct("Kaninører", 200, 700, 34565653421, "Velysion-Market", "345654", produktgruppe)
+        faktura.addOrdrelinje(p1, 2);
+        assert.equal(faktura.ordrelinjer.length, 1);
+    })
+})
